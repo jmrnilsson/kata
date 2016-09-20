4e313df6535cba56dd062beda1f0c830b034f11c
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-09-20 22:14:45 +0200

    using side_effect list for multiple calls

--- tests/04_side_effects_tests.py
+++ tests/04_side_effects_tests.py
@@ -4,7 +4,7 @@ from mock import Mock
 from download import beers
 
 
-def test_retry():
+def test_side_effect_with_list():
     beers.find_all = Mock(side_effect=[0, 1, {'something': True}])
     beers.find_all()
     assert_equal(beers.find_all.call_count, 1)
