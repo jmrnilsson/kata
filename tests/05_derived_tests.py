02cb52d11c637eea29feb0e7f94273e8fda3b554
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-09-21 00:40:52 +0200

    add 05 assert_by_start_date

---
+++ tests/05_derived_tests.py
@@ -0,0 +1,9 @@
+from nose.tools import assert_greater
+
+from download import beers
+
+
+def test_order_by_start_date():
+    actual = beers.find_by_start_date('2016-05-01')[0]
+
+    assert_greater(actual, '2016-04-29')
